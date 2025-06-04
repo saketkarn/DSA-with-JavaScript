5f0047b9bb8c2e64e1777fafd13923a06b728db7
Saket Karn <39449752+saketkarn@users.noreply.github.com>
2025-06-05 02:01:02 +0530

    single no

---
+++ 2) Arrays - Level 0/9) Single number.js
@@ -0,0 +1,13 @@
+let arr=[2,2,1],xor=0;
+for(let i=0;i<arr.length;i++){
+    xor=xor^arr[i]
+}
+console.log(xor)
+
+
+//XOR: 
+
+// 0^0 = 0
+// 0^1 = 1
+
+//So, the duplicates would be removed as xor of same number would be removed.
